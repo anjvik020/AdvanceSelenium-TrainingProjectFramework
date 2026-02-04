d561122eb0277b1cd07e95c0b346ad337314993f
vikas <vikas@Vikash-PC>
2026-02-04 15:57:30 +0530

    ADDED ELEMENT 3

--- AdvanceSelenium/src/main/java/com/comcast/crm/objectrepo/ProductPage.java
+++ AdvanceSelenium/src/main/java/com/comcast/crm/objectrepo/ProductPage.java
@@ -21,6 +21,12 @@ public class ProductPage {
 	{
 		return createProduct1;
 	}
+	@FindBy(xpath = "//span[.='Add Product']")
+	private WebElement createProduct2;
+	public WebElement getProduct2()
+	{
+		return createProduct2;
+	}
 	
 	@FindBy(name="productName")
 	private WebElement productName;
